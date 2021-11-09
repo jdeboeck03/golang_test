8ea94b1379f44d2dd52e648db8fc0c72a21f6641
Jelle De Boeck <jelle.deboeck@axxes.com>
2021-11-09 15:21:31 +0100

    Added postAlbums func

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -14,11 +14,6 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-// get Albums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
 // album slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
@@ -29,5 +24,27 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
 	router.Run("localhost:8080")
+}
+
+// get Albums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJson to bind the received JSON to
+	// newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
